9cce593cc0c75e3957169ea4f1fab1e8f01c5531
unknown <melsom.henrik@gmail.com>
2018-02-28 22:22:29 +0100

    lines work

--- _harp/main.js
+++ _harp/main.js
@@ -1,3 +1,8 @@
-let line = new Line({
+let line1 = new Line({
+    id: 0,
     text: 'Hello, world!',
+})
+let line2 = new Line({
+    id: 1,
+    text: 'Hello, world again!',
 })
--- _harp/src/Line.js
+++ _harp/src/Line.js
@@ -1,17 +1,21 @@
 class Line {
     constructor(options) {
         this.prompt = options.prompt | false
-        this.text = options.text | ""
-        this.id = "line-" + (options.id | 0)
+        this.text = options.text || ''
+        this.id = 'line-' + (options.id | 0)
+        this.class = 'console-line'
+        this.onComplete = options.onComplete || function() {}
 
         this.consoleElement = document.getElementById('console')
 
-        this.element = document.createElement('span')
+        this.element = document.createElement('div')
         this.element.id = this.id
+        this.element.className = this.class
         this.consoleElement.appendChild(this.element)
-
-        if (!this.prompt && this.text !== "") {
-            this.typed = new Typed('#'+this.id, {
+        
+        let searchElem = '#' + this.id
+        if (!this.prompt && this.text !== '') {
+            this.typed = new Typed(searchElem, {
                 strings: [ this.text ],
                 typeSpeed: 30,
                 showCursor: false,
